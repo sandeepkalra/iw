88feebfc7e2c09b497e57eb41e21ba2154523444
Sandeep Kalra <sandeep.kalra@gmail.com>
2014-06-04 09:19:14 -0500

    0.002

--- string_fn.c
+++ string_fn.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int StrLen(const char *s)
+int StrLen( char *p)
 {
   int sz=0;
-  char *p = s;
   while(p && *p!='\0') { sz++; p++;}
   return sz;
 }
@@ -13,6 +12,35 @@ void StrCpy(char *dest, char *src)
   while((*dest++ = *src++) != '\0') { }
 }
 
+int StrCmp( char *p,  char *q) 
+{
+  while(*p!='\0' && *q!= '\0' && (*p++ == *q++)) { }
+  return (*p-*q);
+}
+
+char *StrStr(char *hay, char *needle)
+{
+	int i=0;
+	int sz_h = StrLen(hay); 
+	int sz_n = StrLen(needle);
+	if(sz_h < sz_n) return 0;
+	for(i =0 ; i < sz_h - sz_n + 1; ++i)
+	{
+		int j=0;
+		for(j=0;j<sz_n;++j)
+		{
+			if(hay[i+j]!=needle[j]) break;
+		}
+		if(j==sz_n) return &hay[i];
+	}
+	return 0;
+}
+
+char *StrChr(char *hay, char c)
+{
+	while(*hay!= '\0' && (*hay != c)) { hay++; }
+	return hay;
+}
 
 int main() 
 {
