13c5561c5ad0542e7ffeb6812ac8d48b67bc0515
Sandeep Kalra <sandeep.kalra@gmail.com>
2014-08-09 20:24:58 -0500

    reg_count.cpp modified

--- reg_count.cpp
+++ reg_count.cpp
@@ -8,9 +8,24 @@ using namespace std;
 int main()
 {
 	unordered_map<string, int> cnt;
-	string s = "this is a test. the test has a number of chars that are repeated. the number of occourances must be noticed later";
-	regex pattern {R"(\w+)"};
-	for (sregex_iterator i{s.begin(),s.end(), pattern}; i!=s.end(); ++i)
-		cnt[(*i).str()]++;
-	for(auto i: cnt) cout<<i.first<<","<<i.second<<endl;
+	//string s = "this is a test. the test has a number of chars that are repeated. the number of occourances must be noticed later";
+	string s="10.1.20.299";
+	//regex pattern {R"(\w+)"};
+	//regex  e{R"((\d{1,3})((.{1,3}){3}))"};
+	regex  e{R"((\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3}))"};
+	if(regex_match(s.begin(),s.end(),e)) 
+	{
+		cout<<s<<" matches  ";
+		bool iscorrect = true;
+		// now the check for validity.
+		smatch v_smatch; 
+		regex_match(s,v_smatch,e);
+		for(auto i:v_smatch) {
+				if(i > 255) iscorrect = false;
+		}
+		if(!iscorrect) cout<<" but is not correct\n";
+		else cout<<" and is  correct\n";
+	}
+	else cout<<s<<" do-not match\n";
+	
 }
