6115ba4c027c77a43fc61ce35528dc94cfaf4d61
sandeep kalra <sandeep.kalra@gmail.com>
2014-08-11 11:14:18 -0500

    still working on tries

--- tries.cpp
+++ tries.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
 struct node {
 	char c;
 	bool isEnd; 
 	node *next[256];
-	node(char ch) : c(ch) { memset(next,0, sizeof(node)*256); }
+	node(char ch) : c(ch) { memset(this->next, 0 , sizeof(node*)*256); if(c=='$') isEnd = true; }
 };
 
 static node start('%');
@@ -14,28 +15,31 @@ static node start('%');
 void Insert(const char *c)
 {
 	if(!c) return;
-	char *s = c;
-	node *p = start.next[tolower(*s)];
-	
-	while(*s!='\0') {
-		if(!p) p = new node(*s);
-		s++;
-		if(*s != '\0') p = p->next[tolower(*s)];
+	char *s=const_cast<char*>(c);
+	node *curr = (&start)->next[tolower(*s)];
+	while(*s != '\0') 
+	{
+		if(curr == nullptr) 
+		{ 
+			curr = new node(*s); 
+		}
+		++s; 
+		curr = curr->next[tolower(*s)];
 	}
-	p->isEnd = true;
+	curr = new node('$');
 }
 
 bool Find(const char *c)
 {
-	char *s=c;
+	char *s=const_cast<char*>(c);
 	node *p = start.next[tolower(*s)];
-	while(*s!='\0' && p) { s++; p = p->next[tolower(*s)]; }
+	while(*s!='\0' && p) { p = p->next[tolower(*s)]; ++s; }
 	if(*s=='\0' && p && p->isEnd) return true; else return false;
 }
 
 int main()
 {
-	string names[]={"sandeep","kalra","gurgaon"};
+	string names[]={"sk","sandeep","gurgaon"};
 	for(auto i: names) Insert(i.c_str());
 	cout<<"Find(SANDEEP)="<<Find(string("SANDEEP").c_str())<<endl;
 }
