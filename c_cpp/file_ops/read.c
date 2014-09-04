19c651789a2cf4e1c2e5e9340f684e383f07c332
Sandeep Kalra <Sandeep.kalra@gmail.com>
2014-09-04 11:19:25 -0500

    file operations in c and cpp

---
+++ c_cpp/file_ops/read.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char filename[100];
+	printf("\nEnter the file you want to read: ");
+	scanf(" %[^\t\n]", filename);
+	FILE *fp = fopen(filename, "rw");
+	if(!fp) {
+		printf("Cannot open file. Ending.... \n");
+		return 0;
+	}
+	char buffer[20000];
+	int index_in_buffer = 0; 
+	int bytes_read = 0 ;
+	int cond = 1 ;
+	while(cond) {
+		bytes_read = fread(buffer + index_in_buffer, 1, 100 ,fp);
+		index_in_buffer += bytes_read;
+		if(bytes_read <= 0 ) cond = 0;
+	}
+	buffer[index_in_buffer]='\0';
+
+	printf("File Reading complete. Total bytes = %d", index_in_buffer);
+
+	fclose(fp);
+return 0;
+}
---
+++ c_cpp/file_ops/read.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream> // file stream
+#include <string>
+using namespace std;
+
+
+int main () {
+	cout<<"Enter the file you want to read"<<endl;
+	string  filename;
+	cin>>filename;
+	// create and open the stream
+	fstream in_stream;
+	in_stream.open(filename, fstream::in | fstream::app);
+
+	if(!in_stream)
+	{
+		cout<<"Cannot find file "<<filename<<". Ending... "<<endl;
+		return 0;
+	}
+	in_stream.seekg(0, in_stream.end); // move the cursor to end.
+	int length = in_stream.tellg();
+	in_stream.seekg(0,in_stream.beg); // move back cursor once length is measured.
+	cout<<"file length:"<<length<<endl;
+	char *buffer = new char[length];
+	
+	in_stream.read(buffer, length);
+
+	delete buffer;
+}
+
