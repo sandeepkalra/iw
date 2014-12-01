9bb70d0d57fc64224aa269bb0267f6ea0905a094
Sandeep Kalra <Sandeep.kalra@gmail.com>
2014-11-30 20:31:05 -0600

    leak fixed

--- design_patterns/creational/singleton/main.cpp
+++ design_patterns/creational/singleton/main.cpp
@@ -7,7 +7,7 @@ class Singleton
 private:
    static Singleton *instance;
    string name;
-   Singleton() {}
+   Singleton() { instance = 0 ; }
    Singleton(Singleton const &);
    Singleton & operator=(Singleton const &);
 public:
@@ -16,8 +16,15 @@ public:
 	if(!instance) instance = new Singleton();
 	return instance;
    }
+   void Destroy() {
+	if(instance) delete instance;
+	instance = 0;
+   }
    void SetName(string s) { name = s; }
    string GetName() { return name; }
+   ~Singleton() { 
+		cout<<"~Singleton\n";
+	}
 };
 
 Singleton * Singleton::instance = 0;
@@ -48,4 +55,5 @@ int main() {
 	s1.SetName("A");
 	s2.SetName("B"); // if s1 & s2 are same, a->GetName must be "B"
 	cout<<"A->getname = "<< s1.GetName() << endl; 
+	a->Destroy();
 }
