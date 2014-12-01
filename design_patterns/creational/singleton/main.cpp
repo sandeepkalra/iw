d79be6a8723f97dd83ab6d00128e21d8e5dde0fd
Sandeep Kalra <Sandeep.kalra@gmail.com>
2014-11-30 19:42:52 -0600

    better singleton

--- design_patterns/creational/singleton/main.cpp
+++ design_patterns/creational/singleton/main.cpp
@@ -22,11 +22,30 @@ public:
 
 Singleton * Singleton::instance = 0;
 
+/* Better Choice */
+class S {
+	string name;
+	S() {}
+	S(S const&);
+	S& operator=(S const&);
+public:
+	static S& GetInstance() {
+		static S instance;
+		return instance;
+	}
+   void SetName(string s) { name = s; }
+   string GetName() { return name; }
+	
+	~S(){cout<<"~S\n"; }
+};
 
 int main() {
 	Singleton *a = Singleton::GetInstance();
 	Singleton *b  = Singleton::GetInstance();
-	a->SetName("A");
-	b->SetName("B"); // if a & b are same, a->GetName must be "B"
-	cout<<"A->getname = "<< a->GetName() << endl; 
+	S& s1 = S::GetInstance();
+	S& s2 = S::GetInstance();
+
+	s1.SetName("A");
+	s2.SetName("B"); // if s1 & s2 are same, a->GetName must be "B"
+	cout<<"A->getname = "<< s1.GetName() << endl; 
 }
